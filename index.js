2bdac08f00d86209ee9c97c359655dbd1dc87982
somosa <joma828@gmail.com>
2026-05-11 00:46:39 +0600

    last commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv =require("dotenv")
-const { MongoClient, ServerApiVersion } = require("mongodb");
-dotenv.config()
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+dotenv.config();
 const uri = process.env.MONGODB_URI;
 
 const app = express();
@@ -10,14 +10,13 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 async function run() {
   try {
@@ -25,16 +24,42 @@ async function run() {
 
     const db = client.db("wanderlust");
     const destinationCollection = db.collection("destination");
+    //api link banano
+    app.get("/destinations", async (req, res) => {
+      const result = await destinationCollection.find().toArray();
+      res.send(result);
+    });
+    // data cereate kora
+    app.post("/destinations", async (req, res) => {
+      const destinations = req.body;
+      const result = await destinationCollection.insertOne(destinations);
+      res.json(result);
+    });
 
-app.post("/destinations", async (req, res) => {
-  const destinations = req.body;
-  const result = await destinationCollection.insertOne(destinations);
-  res.json(result);
-});
-app.get("/destinations", async (req, res) => {
-  const result = await destinationCollection.find().toArray();
-  res.send(result);
-});
+    // detills card
+    app.get("/destinations/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await destinationCollection.findOne({ _id: new ObjectId(id) });
+      res.json(result);
+    });
+// Edit data 
+    app.patch("/destinations/:id", async (req, res) => {
+      const { id } = req.params;
+     const updateData =req.body
+     const result =await destinationCollection.updateOne(
+      {_id:new ObjectId(id)},
+      {$set :updateData}
+     )
+     res.json(result)
+    });
+
+    //deleted kora 
+     app.delete("/destinations/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await destinationCollection.deleteOne({ _id: new ObjectId(id) });
+      res.json(result);
+    });
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
